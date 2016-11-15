c87a67681c113a771c87db75b8ecdebba1c5f510
Douglas Caruso <Douglas Caruso>
2016-11-15 16:55:17 -0200

    added MySQL Connection

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var app = require("./config/server.js");
 
 var rotaHome = require("./app/routes/home")(app);
+var rotaInclusao = require("./app/routes/inclusao")(app);
+var rotaNoticias = require("./app/routes/noticias")(app);
 
 app.listen(3000, function(){
     console.log("Server ON");
--- app/routes/inclusao.js
+++ app/routes/inclusao.js
@@ -1,3 +1,5 @@
-app.get('/inclusao_noticia', function(req, res){
-    res.render("admin/form_add_noticia.ejs");
-});
+module.exports = function(app){
+    app.get('/inclusao_noticia', function(req, res){
+        res.render("admin/form_add_noticia.ejs");
+    });
+}
