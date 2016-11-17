3ad0cae3c37dbf884a0acf5a8f4aa2efcbd5f355
Douglas Caruso <Douglas Caruso>
2016-11-17 17:04:47 -0200

    Added Consign, Post, ...

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,4 +2,10 @@ module.exports = function(app){
     app.get('/inclusao_noticia', function(req, res){
         res.render("admin/form_add_noticia.ejs");
     });
+
+    app.post('/noticias/salvar', function(req, res){
+        
+        var noticias = req.body;
+        res.send(noticias.noticia);
+    });
 }
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,15 +1,13 @@
+//var dbConnection = require('../../config/dbConnection');
+
 module.exports = function(app){
     app.get('/noticias', function(req, res){
-        var mysql = require("mysql")
-        
-        var connection = mysql.createConnection({
-            host : 'localhost',
-            user : 'root',
-            password : '1234',
-            database : 'portal_noticias'
-        });
+     
+        var connection = app.config.dbConnection();        
+        //var connection = dbConnection();
 
-        connection.query('select * from noticias', function(error, result){
+        var noticiasModel = app.app.models.noticiasModel;
+        noticiasModel.getNoticias(connection, function(error, result){
             res.render("noticias/noticias", {noticias : result});
         });
     });
